7f8767201f9750abb9523f0316b1b367502a8661
XEON <XEON@DESKTOP-MON8CM0>
2024-12-10 19:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosWhile1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Console.WriteLine("Digite a sua senha: ");
+                int senha = int.Parse(Console.ReadLine());
+
+                while (senha != 2270) {
+                    Console.WriteLine("Senha Invalida: ");
+                    senha = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Acesso permitido");
+
+            }
+        }
+    }
+}
